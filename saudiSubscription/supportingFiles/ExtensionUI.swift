d6f3a130aa17e90fc280971b3c18aa97cc4e97b4
devAshwaq <88581485+devAshwaq@users.noreply.github.com>
2022-12-23 23:30:35 +0300

    added UI extension file

---
+++ saudiSubscription/supportingFiles/ExtensionUI.swift
@@ -0,0 +1,98 @@
+//
+//  ExtensionUI.swift
+//  saudiSubscription
+//
+//  Created by Ashwaq Alghamdi on 8.12.2022.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+    public func setGradientBackgroundForCard() {
+        let gradientLayer = CAGradientLayer()
+        let middleColor = #colorLiteral(red: 0.5960784314, green: 0.6862745098, blue: 0.737254902, alpha: 1).cgColor
+        let buttomColor = #colorLiteral(red: 0.05882352963, green: 0.180392161, blue: 0.2470588237, alpha: 1).cgColor
+        gradientLayer.colors = [buttomColor, middleColor]
+        gradientLayer.startPoint = CGPoint(x: 1.0, y: 1.0)
+        gradientLayer.endPoint = CGPoint(x: 1.0, y: 0.0)
+        gradientLayer.frame = self.frame
+        gradientLayer.frame.size = self.frame.size
+        self.layer.insertSublayer(gradientLayer, at:0)
+    }
+}
+
+extension UILabel {
+
+    func flash() {
+        // Take as snapshot of the button and render as a template
+        let snapshot = self.snapshot?.withRenderingMode(.alwaysTemplate)
+        let imageView = UIImageView(image: snapshot)
+        // Add it image view and render close to white
+        imageView.tintColor = UIColor(white: 0.9, alpha: 1.0)
+        guard let image = imageView.snapshot  else { return }
+        let width = image.size.width
+        let height = image.size.height
+        // Create CALayer and add light content to it
+        let shineLayer = CALayer()
+        shineLayer.contents = image.cgImage
+        shineLayer.frame = bounds
+
+        // create CAGradientLayer that will act as mask clear = not shown, opaque = rendered
+        // Adjust gradient to increase width and angle of highlight
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [UIColor.clear.cgColor,
+                                UIColor.clear.cgColor,
+                                UIColor.black.cgColor,
+                                UIColor.clear.cgColor,
+                                UIColor.clear.cgColor]
+        gradientLayer.locations = [0.0, 0.35, 0.50, 0.65, 0.0]
+        gradientLayer.startPoint = CGPoint(x: 0.0, y: 0.0)
+        gradientLayer.endPoint = CGPoint(x: 1.0, y: 0.0)
+
+        gradientLayer.frame = CGRect(x: -width, y: 0, width: width, height: height)
+        // Create CA animation that will move mask from outside bounds left to outside bounds right
+        let animation = CABasicAnimation(keyPath: "position.x")
+        animation.byValue = width * 2
+        // How long it takes for glare to move across button
+        animation.duration = 5
+        // Repeat forever
+        animation.repeatCount = Float.greatestFiniteMagnitude
+        animation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeInEaseOut)
+
+        layer.addSublayer(shineLayer)
+        shineLayer.mask = gradientLayer
+
+        // Add animation
+        gradientLayer.add(animation, forKey: "shine")
+    }
+}
+
+extension UIView {
+    // Helper to snapshot a view
+    var snapshot: UIImage? {
+        let renderer = UIGraphicsImageRenderer(size: bounds.size)
+
+        let image = renderer.image { context in
+            layer.render(in: context.cgContext)
+        }
+        return image
+    }
+}
+
+extension Date {
+    static func - (lhs: Date, rhs: Date) -> TimeInterval {
+        return lhs.timeIntervalSinceReferenceDate - rhs.timeIntervalSinceReferenceDate
+    }
+}
+
+extension TimeInterval {
+    func asMinutes() -> Double { return self / (60.0) }
+    func asHours()   -> Double { return self / (60.0 * 60.0) }
+    func asDays()    -> Double { return self / (60.0 * 60.0 * 24.0) }
+    func asWeeks()   -> Double { return self / (60.0 * 60.0 * 24.0 * 7.0) }
+    func asMonths()  -> Double { return self / (60.0 * 60.0 * 24.0 * 30.4369) }
+    func asYears()   -> Double { return self / (60.0 * 60.0 * 24.0 * 365.2422) }
+}
+
+
